33dd331c61d322de2eff1ab2131033af0c931987
Killa <69673342+killa-killa@users.noreply.github.com>
2023-07-16 19:51:14 +1000

    Add files via upload

---
+++ widget.js
@@ -0,0 +1,8 @@
+// JavaScript code for calendar widget functionality
+// ...
+
+// Example: Generating a calendar using JavaScript
+const calendarWidget = document.getElementById('calendar-widget');
+
+// Add your calendar generation logic here
+// ...

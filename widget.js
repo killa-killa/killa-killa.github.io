a9ed9630ec5e3e5a2c42744fdf59f5e4c6087e1f
Killa <69673342+killa-killa@users.noreply.github.com>
2023-07-16 19:57:05 +1000

    Delete widget.js

--- widget.js
+++
@@ -1,8 +0,0 @@
-// JavaScript code for calendar widget functionality
-// ...
-
-// Example: Generating a calendar using JavaScript
-const calendarWidget = document.getElementById('calendar-widget');
-
-// Add your calendar generation logic here
-// ...
